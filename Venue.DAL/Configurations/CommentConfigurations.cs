50fdaeeebc75248aebe2a5f4b5d917823dd46db4
Aiganysh Talantbekova <ramonaramonr@gmail.com>
2020-10-17 10:10:27 +0600

    Created configurations for entities and created migration

---
+++ Venue.DAL/Configurations/CommentConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Venue.DAL.Entities;
+
+namespace Venue.DAL.Configurations
+{
+    public class CommentConfigurations : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.HasOne(x => x.User)
+                .WithMany(y => y.Comments)
+                .HasForeignKey(x => x.UserId);
+
+            builder.HasOne(x => x.Venue)
+                .WithMany(y => y.Comments)
+                .HasForeignKey(x => x.VenueId);
+        }
+    }
+}
