e8f43171a7e9efaf6873a054b78277a527ba3b85
Aiganysh Talantbekova <ramonaramonr@gmail.com>
2020-10-17 09:40:59 +0600

    Realized IBaseEntity interface

--- Venue.DAL/Entities/BaseEntity.cs
+++ Venue.DAL/Entities/BaseEntity.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Venue.DAL.Entities
 {
     public class BaseEntity<T> : IBaseEntity<T>
     {
         public T Id { get; set; }
+        public DateTime? UpdatedUtc { get; set; }
+        public DateTime CreatedUtc { get; set; }
     }
 }
